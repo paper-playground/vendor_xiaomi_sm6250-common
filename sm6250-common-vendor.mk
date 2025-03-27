9172d2024e4cff2b5eea3e13fb64741ac1676fbf
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-03-27 11:27:55 +0000

    Revert "sm6250-common: audio: configuration for spatial audio [2/2]"
    This reverts commit 1267b0192019a3ae3a4ca1576da5cbf5dec6939d.

--- sm6250-common-vendor.mk
+++ sm6250-common-vendor.mk
@@ -430,7 +430,6 @@ PRODUCT_PACKAGES += \
     libqcreverb \
     libqcvirt \
     libshoebox \
-    libspatialaudio \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
     com.qti.eeprom.sunny_gc8034 \
     com.qti.sensor.curtana_gc02m1 \
