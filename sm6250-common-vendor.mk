e5dfe494b51faddeb14b7e28211429c35513b42b
sajidshahriar72543 <sazidshahriar39@gmail.com>
2025-03-27 11:28:05 +0000

    Revert "sm6250-common: audio: Import QCOM implementation audio effects blobs"
    This reverts commit 9af40dd2ab3de7003560374f373cac5be2e595ff.

--- sm6250-common-vendor.mk
+++ sm6250-common-vendor.mk
@@ -426,9 +426,6 @@ PRODUCT_PACKAGES += \
     libwfduibcsrcinterface_proprietary \
     libwfdutils_proprietary \
     libasphere \
-    libqcbassboost \
-    libqcreverb \
-    libqcvirt \
     libshoebox \
     vendor.qti.hardware.wifidisplaysessionl@1.0-halimpl \
     com.qti.eeprom.sunny_gc8034 \
